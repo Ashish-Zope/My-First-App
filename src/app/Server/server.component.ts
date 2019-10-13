4b79cb0ec8121d0be490addf004162d270605e5b
Ashish <ashish.zope93@gmail.com>
2019-10-13 13:30:48 +0530

    initial commit

---
+++ src/app/Server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'App-Server',
+    templateUrl:'./Server.component.html'
+})
+export class ServerComponent{
+
+}
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '.app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit{
+  allowNewServer=false;
+  serverCerationStatus='No Server Created';
+  serverName='test server name';
+  userName='';
+  userStatus='';
+  serverCreated:boolean=false;
+  Servers=['TestServer']
+  ngOnInit(){}
+  constructor()
+  {
+    setTimeout(()=>{
+    this.allowNewServer=true;
+  },2000);
+
+}
+userId:number=1;
+getServerStatus() {
+  return 'Active';
+  
+}
+onCreateServer()
+{
+  this.serverCerationStatus='Server is created'+ this.serverName ;
+  this.serverCreated=true;
+  this.Servers.push(this.serverName);
+}
+
+OnServerNameUpdate(event:Event)
+{
+  //console.log(event)
+  this.serverName=(<HTMLInputElement>event.target).value;
+}
+
+onUserBtnClick()
+{
+  this.userStatus='User is created';
+}
+
+
+}
